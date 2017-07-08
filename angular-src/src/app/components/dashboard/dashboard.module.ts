1d0b0687c3037d9e91532ecf2fb0b7fad3fb1bb0
Shao Hui <shinlos@hotmail.com>
2017-07-09 04:13:22 +0800

    add froala wysiwyg

--- angular-src/src/app/components/dashboard/dashboard.module.ts
+++ angular-src/src/app/components/dashboard/dashboard.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+
 import { DashboardRouter } from './dashboard-router';
 
 import { DashboardComponent } from './dashboard.component';
@@ -11,8 +13,19 @@ import { OverviewComponent } from './overview/overview.component';
 import { MediaComponent } from './media/media.component';
 
 @NgModule({
-    imports: [CommonModule, DashboardRouter],
-    declarations: [DashboardComponent, SidebarComponent, PostComponent, OverviewComponent, MediaComponent],
+    imports: [
+        CommonModule, 
+        FroalaEditorModule.forRoot(), 
+        FroalaViewModule.forRoot(), 
+        DashboardRouter
+    ],
+    declarations: [
+        DashboardComponent, 
+        SidebarComponent, 
+        PostComponent, 
+        OverviewComponent, 
+        MediaComponent
+    ],
     exports: [],
     providers: []
 })
